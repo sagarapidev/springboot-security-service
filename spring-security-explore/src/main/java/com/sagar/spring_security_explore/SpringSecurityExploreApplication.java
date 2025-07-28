e115a734b57ae431185f081116a356da8f07115c
sagarapidev <sagar.api.dev@gmail.com>
2025-07-29 00:14:34 +0530

    scaffold spring boot

---
+++ spring-security-explore/src/main/java/com/sagar/spring_security_explore/SpringSecurityExploreApplication.java
@@ -0,0 +1,13 @@
+package com.sagar.spring_security_explore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityExploreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityExploreApplication.class, args);
+    }
+
+}
---
+++ spring-security-explore/src/test/java/com/sagar/spring_security_explore/SpringSecurityExploreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagar.spring_security_explore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityExploreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
