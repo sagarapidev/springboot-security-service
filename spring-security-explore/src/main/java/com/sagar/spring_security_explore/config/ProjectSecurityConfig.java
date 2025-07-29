afe5db2b115c42c53bc744c17978a2711542fa8a
sagarapidev <sagar.api.dev@gmail.com>
2025-07-30 02:40:20 +0530

    #4

--- spring-security-explore/src/main/java/com/sagar/spring_security_explore/config/ProjectSecurityConfig.java
+++ spring-security-explore/src/main/java/com/sagar/spring_security_explore/config/ProjectSecurityConfig.java
@@ -2,6 +2,7 @@ package com.sagar.spring_security_explore.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,6 +12,7 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -49,6 +51,8 @@ public class ProjectSecurityConfig {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
-    ;
-
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker() {
+        return new HaveIBeenPwnedRestApiPasswordChecker();
+    }
 }
