beae650cdcdfd4e5e970b0a10df586e11d02371f
sagarapidev <sagar.api.dev@gmail.com>
2025-07-31 23:52:50 +0530

    Refactor validation message imports in Customer entity
    Replaces fully qualified references to validation messages with static imports from ValidationMessage. This improves code readability and consistency in the Customer entity class.

--- spring-security-explore/src/main/java/com/sagar/spring_security_explore/entity/Customer.java
+++ spring-security-explore/src/main/java/com/sagar/spring_security_explore/entity/Customer.java
@@ -18,6 +18,7 @@ import static com.sagar.spring_security_explore.constant.DatabaseTable.CUSTOMER_
 import static com.sagar.spring_security_explore.constant.DatabaseTable.CUSTOMER_EMAIL;
 import static com.sagar.spring_security_explore.constant.DatabaseTable.CUSTOMER_ROLE;
 import static com.sagar.spring_security_explore.constant.DatabaseTable.CUSTOMER_HASH_PWD;
+import static com.sagar.spring_security_explore.constant.ValidationMessage.*;
 
 @Getter
 @Setter
@@ -26,24 +27,24 @@ import static com.sagar.spring_security_explore.constant.DatabaseTable.CUSTOMER_
 @Entity
 @Table(name = CUSTOMER)
 public class Customer extends BaseEntity {
-    @NotBlank(message = ValidationMessage.NAME_REQUIRED)
-    @Size(max = 100, message = ValidationMessage.NAME_MAX)
+    @NotBlank(message = NAME_REQUIRED)
+    @Size(max = 100, message = NAME_MAX)
     @Column(name = CUSTOMER_NAME, nullable = false, length = 100)
     String name;
 
-    @NotBlank(message = ValidationMessage.EMAIL_REQUIRED)
-    @Email(message = ValidationMessage.EMAIL_VALID)
-    @Size(max = 100, message = ValidationMessage.EMAIL_MAX)
+    @NotBlank(message = EMAIL_REQUIRED)
+    @Email(message = EMAIL_VALID)
+    @Size(max = 100, message = EMAIL_MAX)
     @Column(name = CUSTOMER_EMAIL, nullable = false, length = 100)
     String email;
 
-    @NotBlank(message = ValidationMessage.ROLE_REQUIRED)
-    @Size(max = 50, message = ValidationMessage.ROLE_MAX)
+    @NotBlank(message = ROLE_REQUIRED)
+    @Size(max = 50, message = ROLE_MAX)
     @Column(name = CUSTOMER_ROLE, nullable = false, length = 50)
     String role;
 
-    @NotBlank(message = ValidationMessage.PASSWORD_HASH_REQUIRED)
-    @Size(max = 255, message = ValidationMessage.PASSWORD_HASH_MAX)
+    @NotBlank(message = PASSWORD_HASH_REQUIRED)
+    @Size(max = 255, message = PASSWORD_HASH_MAX)
     @Column(name = CUSTOMER_HASH_PWD, nullable = false, length = 255)
     String hashPwd;
 }
